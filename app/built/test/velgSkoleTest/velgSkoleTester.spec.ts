35d99bcbf7337bb847d28676c6cbe439bae55385
Lars-Espen Kolberg <larsespen9@gmail.com>
2016-10-17 19:07:40 +0200

    Prøver å lage en test på skoleListeFilterPipe.ts , men den fungerer ikke..

--- app/built/test/velgSkoleTest/velgSkoleTester.spec.ts
+++ app/built/test/velgSkoleTest/velgSkoleTester.spec.ts
@@ -1,3 +1,20 @@
+/*
+import { SkoleListeFilterPipe} from'../../../velgSkole/skoleListeFilter.pipe';
+import { Skole } from'../../../velgSkole/skole';
+//Test variabler
+
+
 describe('skoleListeFilterPipe', () => {
-  it('true is true', () => expect(true).toBe(true));
+  let skoler : Array<any>;
+  skoler = [{"skole":"Auglend skole"},{"skole":"Austbø skole"},{"skole":"Buøy skole","TrykketPa":true},{"skole":"Byfjord skole","TrykketPa":false}];
+  let skolerResultat: Array<any>;
+  skolerResultat = [{"skole":"Auglend skole"}];
+
+  let skolenavn = "auglend";
+  // This pipe is a pure, stateless function so no need for BeforeEach
+  let pipe = new SkoleListeFilterPipe();
+  it('transforms "abc" to "Abc"', () => {
+    expect(pipe.transform(skoler, skolenavn)).toBe(skolerResultat);
+  });
 });
+*/
