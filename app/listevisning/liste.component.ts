d7ec5ea1758bc39bddc445012dd13b8b03180c9f
piiiias <piiiias@hotmail.com>
2016-10-18 13:00:44 +0200

    no message

--- app/listevisning/liste.component.ts
+++ app/listevisning/liste.component.ts
@@ -18,8 +18,8 @@ import { ValgteSkolerService } from '../valgteSkoler.service';
     <ul>
         <li><p *ngFor="let dato of datoArray">{{dato}}</p></li>
         <li><p *ngFor="let rute1 of this.skole1"> {{rute1.kommentar}} </p></li>
-        <li><p *ngFor="let rute2 of skole2">{{rute2.kommentar}} </p></li>
-        <li><p *ngFor="let rute3 of skole3">{{rute3.kommentar}} </p></li>
+        <li><p *ngFor="let rute2 of this.skole2"> {{rute2.kommentar}} </p></li>
+        <li><p *ngFor="let rute3 of this.skole3"> {{rute3.kommentar}} </p></li>
     </ul>`
 })
 export class ListeComponent implements OnInit, OnDestroy {
@@ -33,8 +33,13 @@ export class ListeComponent implements OnInit, OnDestroy {
 
     ngOnInit() {
       this.valgteSkoleRuter = this.valgteSkolerService.delteValgteSkoleRuter;
-      this.skole1 = this.listeService.nesteSkole(this.valgteSkolerService.delteValgteSkoleRuter, this.skole1, this.skole2, this.skole3);
+      this.skole1 = this.listeService.skole1(this.valgteSkolerService.delteValgteSkoleRuter, this.skole1);
+      this.skole2 = this.listeService.skole2(this.valgteSkolerService.delteValgteSkoleRuter, this.skole2);
+      this.skole3 = this.listeService.skole3(this.valgteSkolerService.delteValgteSkoleRuter, this.skole3);
       this.datoArray = this.listeService.datoer(this.valgteSkolerService.delteValgteSkoleRuter, this.datoArray);
+      console.log(this.skole1);      
+      console.log(this.skole2);
+      console.log(this.skole3);
     }
     
     ngOnDestroy(){
--- app/listevisning/liste.service.ts
+++ app/listevisning/liste.service.ts
@@ -1,10 +1,11 @@
 import { Injectable, Attribute, OnInit, OnDestroy } from '@angular/core';
-
+import { ListeComponent } from './liste.component';
 import { ValgteSkolerService } from './../valgteSkoler.service';
 
 @Injectable()
 
 export class ListeService{
+    
 
     nesteSkole(valgteSkoleRuter:Array<any>,skole1: Array<any>,skole2: Array<any>,skole3: Array<any>){
         var antallValgteSkoler = 1;
@@ -34,20 +35,102 @@ export class ListeService{
         return skole1, skole2, skole3;
     }/*del opp og lag en metode som lager hver skole array */
 
+    skole1(valgteSkoleRuter:Array<any>,skolerute: Array<any>){
+        var g = 0;
+        var antallValgteSkoler = 1;
+        
+        for (var i = 0; i < valgteSkoleRuter.length; i++){
+            
+            if(i > 0){
+            if(valgteSkoleRuter[i-1].skole != valgteSkoleRuter[i].skole){
+                antallValgteSkoler ++;
+            }
+            }
+            if(antallValgteSkoler == 1){
+                skolerute[g] = valgteSkoleRuter[i];
+                g++;
+            }
+        }
+        
+        return skolerute;
+    }
+
+    skole2(valgteSkoleRuter:Array<any>,skolerute: Array<any>){
+        var g = 0;
+        var antallValgteSkoler = 1;
+        for (var i = 0; i < valgteSkoleRuter.length; i++){
+            
+            if(i > 0){
+            if(valgteSkoleRuter[i-1].skole != valgteSkoleRuter[i].skole){
+                antallValgteSkoler ++;
+            }
+            }
+            if(antallValgteSkoler == 2){
+                skolerute[g] = valgteSkoleRuter[i];
+                g++;
+            }
+        }
+        return skolerute;
+    }
+
+    skole3(valgteSkoleRuter:Array<any>,skolerute: Array<any>){
+        var g = 0;
+        var antallValgteSkoler = 1;
+        for (var i = 0; i < valgteSkoleRuter.length; i++){
+            
+            if(i > 0){
+            if(valgteSkoleRuter[i-1].skole != valgteSkoleRuter[i].skole){
+                antallValgteSkoler ++;
+            }
+            }
+            if(antallValgteSkoler == 3){
+                skolerute[g] = valgteSkoleRuter[i];
+                g++;
+            }
+        }
+        return skolerute;
+    }
+
     datoer(valgteSkoleRuter:Array<any>, datoArray: Array<any>){
         for (var i = 0; i < valgteSkoleRuter.length; i++){
             datoArray[i] = valgteSkoleRuter[i].dato;
         }
 
         datoArray = datoArray.sort();
-        for (var i = 1; i < valgteSkoleRuter.length; i++){
-            if (datoArray[i].equals(datoArray[i-1])){
+        /*for (var i = 0; i < valgteSkoleRuter.length; i++){
+            datoArray[i] = new Date(datoArray[i]);
+        }*/
+       /* for (var i = 1; i < valgteSkoleRuter.length; i++){  // funker ikke, Hvordan sammmenligne datoer eller strenger?
+            if (datoArray[i-1] === datoArray[i]){
                 datoArray.splice(i);
             }
 
+        }*/
+        let datoArrayFiltrert = Object.keys(datoArray.reduce((c,a) => {c[a] = true; return c;}))
+
+
+        /*return datoArray;*/
+    }
+
+    sjekkOmSkoleHarDato(valgteSkoleRuter:Array<any>, skole: Array<any>){
+        var skolesortert: Array<any> = [];
+        var datoArray: Array<any> = [];
+        for (var i = 0; i < valgteSkoleRuter.length; i++){
+            datoArray[i] = valgteSkoleRuter[i].dato;
         }
 
-        return datoArray;
+        datoArray = datoArray.sort();
+
+
+        for (var i = 0; i < datoArray.length; i++){
+            for(var j = 0; j < skole.length; j++){
+                if (skole[j].dato == datoArray[i]){  // vil ikke funke før jeg har funnet en måte å sammenligne datoer/strenger
+                    skolesortert[i] = skole[j];
+                }else{
+                    skolesortert[i] = {}
+                }
+            }
+        }
     }
 
     constructor(private valgteSkolerService: ValgteSkolerService) { }
