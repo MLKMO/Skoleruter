08947833c592d7e0d6d424e50aa4e293060aaf73
Ørjan Mæhre <orjan@mehre.no>
2016-10-23 02:50:33 +0200

    Skrevet tester for piper og servicer

--- test/test1/sanity_test.spec.ts
+++ test/test1/sanity_test.spec.ts
@@ -1,22 +1,22 @@
 import { async, TestBed, ComponentFixture } from '@angular/core/testing';
 import { Component, CUSTOM_ELEMENTS_SCHEMA } from '@angular/core';
 
-  beforeEach(async(() => {
+describe('Test av testing', () => {
+    beforeEach(() => {
     TestBed.configureTestingModule({
           declarations: [],
           schemas: [CUSTOM_ELEMENTS_SCHEMA]
     });
        TestBed.compileComponents();
-  }));
+  });
 
-describe('universal truths', () => {
-  it('should do math', () => {
+  it('Skal gjøre matten', () => {
     expect(1 + 1).toEqual(2);
 
     expect(5).toBeGreaterThan(4);
   });
 
-  xit('should skip this', () => {
-    expect(4).toEqual(40);
+  xit('Skal droppe denne (xit)', () => {
+    expect(4).toEqual(4);
   });
 });
