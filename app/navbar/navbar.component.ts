703f116f45c3299d7d12a8cf86c32eaf31f68710
Ørjan Mæhre <orjan@mehre.no>
2016-11-07 03:07:21 +0100

    Legg til kalender er skjult til skoler er valgt

--- app/navbar/navbar.component.ts
+++ app/navbar/navbar.component.ts
@@ -1,4 +1,5 @@
 import { Component } from '@angular/core';
+import { Router, NavigationStart, NavigationEnd, Event as NavigationEvent } from '@angular/router';
 
 
 @Component({
@@ -6,7 +7,21 @@ import { Component } from '@angular/core';
     templateUrl: 'app/navbar/html/navbar.component.html'
 })
 export class NavbarComponent{
-    private skolerValgt: boolean = true;
-    constructor() { }
+    private skolerValgt: boolean = false;
+
+    constructor(private router: Router) 
+    {
+        router.events.subscribe((val) => {
+            if(location.href.includes('skoleliste') || location.href.includes('info'))
+            {
+                this.skolerValgt = false;
+            }
+            else
+            {
+                this.skolerValgt = true;
+            }
+    });
+        
+    }
 
 }
