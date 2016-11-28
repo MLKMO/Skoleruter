01ad003dcc19b041a98c6d6d9e829c1717452289
piiiias <piiiias@hotmail.com>
2016-11-28 13:00:37 +0100

    fjernet en funksjon som ikke var ibruk

--- app/listevisning/liste.service.ts
+++ app/listevisning/liste.service.ts
@@ -6,35 +6,6 @@ import { NesteFridagService } from './../nestefridag/neste-fridag.service';
 @Injectable()
 
 export class ListeService{
-    
-
-    nesteSkole(valgteSkoleRuter:Array<any>,skole1: Array<any>,skole2: Array<any>,skole3: Array<any>){
-        var antallValgteSkoler = 1;
-        var j = 0;
-        var h = 0;
-        var g = 0;
-        
-
-        for (var i = 0; i < valgteSkoleRuter.length; i++){
-            
-            if(i > 0){
-            if(valgteSkoleRuter[i-1].skole != valgteSkoleRuter[i].skole){
-                antallValgteSkoler ++;
-            }
-            }
-            if(antallValgteSkoler == 1){
-                skole1[g] = valgteSkoleRuter[i];
-                g++;
-            }else if(antallValgteSkoler == 2){
-                skole2[j] = valgteSkoleRuter[i];
-                j ++;
-            }else if(antallValgteSkoler == 3){
-                skole3[h] = valgteSkoleRuter[i];
-                h++;
-            }
-        }
-        return skole1, skole2, skole3;
-    }
 
     skole1(valgteSkoleRuter:Array<any>,skolerute: Array<any>){
         var g = 0;
